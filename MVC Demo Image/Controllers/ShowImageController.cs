afddef5d96213d9cc1d98587390dbfb1d8eb3740
Harshdeep Sompura <sompuraharshdeep@gmail.com>
2019-09-06 18:57:58 +0530

    Add project files.

---
+++ MVC Demo Image/Controllers/ShowImageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace MVC_Demo_Image.Controllers
+{
+    public class ShowImageController : Controller
+    {
+        // GET: ShowImage
+        public ActionResult GetImageFromByteArray()
+        {
+            // Get image path  
+            string imgPath = Server.MapPath("~/Content/Images/Ganesh.jpg");
+            // Convert image to byte array  
+            byte[] byteData = System.IO.File.ReadAllBytes(imgPath);
+            //Convert byte arry to base64string   
+            string imgBase64Data = Convert.ToBase64String(byteData);
+            string imgDataURL = string.Format("data:image/jpg;base64,{0}", imgBase64Data);
+            //Passing image data in viewbag to view  
+            ViewBag.ImageData = imgDataURL;
+            return View();
+        }
+    }
+}
---
+++ MVC Demo Image/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Demo_Image.MvcApplication" Language="C#" %>
